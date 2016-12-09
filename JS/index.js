525ec5265fb27f155abda9565923f51f2361205f
Jitendra Pal <jitendra.pal@jasperindia.local>
2016-12-09 16:00:50 +0530

    updated

--- JS/index.js
+++ JS/index.js
@@ -5,3 +5,26 @@ JP.main = (function(){
     
     return {};
 })();
+
+
+JP.chat = (function(){
+    var config = {
+        timer: 3000,
+        timerId: 0
+    }
+    $zopim(function() {
+        $zopim.livechat.window.setColor('#FFCC00');
+        $(".zopim").on("touchstart", function(){ 
+            console.log("Touch start"); 
+            config.timerId = setTimeout(function(){ 
+                $(".zopim").hide(); 
+            }, config.timer); 
+        });
+
+        $(".zopim").on("touchend", function(){ 
+            console.log("Touch end");
+            clearTimeout(config.timerId);
+        });
+
+    });
+})();
